82ce43efe01b2b69d9ea6813d6d399e7cd3fcf5b
pavlokucheriavyi <kuchriavy10@gmail.com>
2022-06-15 22:39:47 +0300

    Final commit

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,5 +1,19 @@
 from .cart import Cart
+from users.models import Profile
 
 
 def cart(request):
-    return {'cart': Cart(request)}
+    flag = ''
+    if request.user.is_authenticated:
+        id_of_current_user = request.user.id
+        get_object_user = Profile.objects.get(user_id=id_of_current_user)
+
+        new_or_not = get_object_user.is_new_user
+        if new_or_not:
+            flag = True
+
+    sections = 'Old_user'
+    if flag:
+        sections = 'New_user'
+
+    return {'cart': Cart(request), 'sections': sections}
