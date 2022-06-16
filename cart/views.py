53742ec9d6c7eb09886a392190481ca952b70a93
pavlokucheriavyi <kuchriavy10@gmail.com>
2022-06-16 23:27:02 +0300

    Send mail added

--- cart/views.py
+++ cart/views.py
@@ -44,7 +44,7 @@ def cart_add(request, product_id):
 
     y = dict(request.POST)
     try:
-        send_mail('Message from Victor', 'Its last message for sendgrid', 'kobra1903@ukr.net', ['kucheriavy10@icloud.com'], fail_silently=True)
+        send_mail('Message from Victor', 'Its last message for sendgrid', 'kobra1903@ukr.net', ['kuchriavy10@gmail.com'], fail_silently=True)
         print('All good with message')
     except BadHeaderError:
         print('Invalid message ')
